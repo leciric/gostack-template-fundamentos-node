114447f698ed5afce9f6a12c73718c58bd1386d5
Leandro Ciric <lecirics@gmail.com>
2020-06-28 16:44:14 -0300

    Get route working but not finished yet

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,50 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income = 0;
+    let outcome = 0;
+
+    const agruparPor = (
+      objetoArray: Array<Transaction>,
+      propriedade: string,
+    ): Transaction => {
+      return objetoArray.reduce((acumulador: Transaction, obj: Transaction) => {
+        const key = obj[propriedade];
+        if (!acumulador[key]) {
+          acumulador[key] = Transaction[];
+        }
+        if (key === 'income') {
+          income += obj.value;
+        } else if (key === 'outcome') {
+          outcome += obj.value;
+        }
+        return acumulador;
+      });
+    };
+
+    const prop: keyof Transaction = 'type';
+
+    agruparPor(this.transactions, prop);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Request): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
